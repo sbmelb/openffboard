f3ffba867a79c3a9cb8ed586161e93820a13bd80
sbmelb <160711715+sbmelb@users.noreply.github.com>
2026-02-06 10:48:21 +1100

    Update main.c

--- Firmware/Targets/F407VG_devboard/Core/Src/main.c
+++ Firmware/Targets/F407VG_devboard/Core/Src/main.c
@@ -150,22 +150,7 @@ int main(void)
   /* Configure the system clock */
   SystemClock_Config();
 
-// TEMP TEST: force PA0/PA1 as GPIO inputs with pull-ups so they read ~3.3V on a multimeter.
-__HAL_RCC_GPIOA_CLK_ENABLE();
 
-GPIO_InitTypeDef GPIO_InitStruct = {0};
-GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
-GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-GPIO_InitStruct.Pull = GPIO_NOPULL;
-GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-while (1)
-{
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0 | GPIO_PIN_1);
-    HAL_Delay(500);
-}
-// end of temp test
 
 
   /* USER CODE BEGIN SysInit */
